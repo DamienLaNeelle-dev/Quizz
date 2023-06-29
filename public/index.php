a94d9348e37a3db50d71e13a499fa1f98caaea8d
Damien L <damien.laneelle33@gmail.com>
2023-06-29 13:26:09 +0200

    add rooting

--- public/index.php
+++ public/index.php
@@ -1,9 +1,32 @@
 <?php
 
-use App\Kernel;
+// public/index.php
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\ErrorHandler\Debug;
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+require dirname(__DIR__).'/vendor/autoload.php';
+
+if ($_SERVER['APP_DEBUG']) {
+    umask(0000);
+
+    Debug::enable();
+}
+
+// Rediriger toutes les requêtes vers l'application React
+if (preg_match('/\.(?:css|js|jpg|png|gif|ico|json)$/', $_SERVER['REQUEST_URI'])) {
+    return false; // Laisser Symfony servir les fichiers statiques
+}
+
+// Rediriger toutes les autres requêtes vers l'application React
+include_once 'index.html';
+
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
+
+
